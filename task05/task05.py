05af84b27e5858af6231a4dfe57d2c7e02ded25e
andrei68o <andrei.olariu2001@gmail.com>
2023-05-16 16:33:18 +0300

    task5update

---
+++ task05/task05.py
@@ -0,0 +1,16 @@
+f = open("input.txt", 'r')
+txt = f.readline()
+f.close()
+dic={}
+no_of_chars = 0
+for ch in txt:
+    if ch.isalpha():
+        no_of_chars += 1
+        if ch in dic.keys():
+            dic[ch] += 1
+        else:
+            dic[ch] = 1
+for key in dic.keys():
+    dic[key] = dic[key] / no_of_chars
+
+print(dic)
